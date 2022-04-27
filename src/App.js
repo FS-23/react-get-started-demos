09520b8a81e4b65e14f98e97cb945b58c9d6091f
dodjidev <dadevcontact@gmail.com>
2022-04-27 13:02:11 +0000

    clean default app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import './css/index.css'
 import Home from './pages/Home.js'
 import Contact from './pages/Contact'
-import TaskList  from './components/TaskList'
 function App() {
   
   return (
     <div className="bg-danger">
          <Home />
-         <TaskList />
     </div>
   );
 }
